b64598da1f23356d2dcbf68d26657e0b1f1ea360
Ghazanfar Ali <gak_92@hotmail.com>
2025-07-17 13:45:19 +0300

    Create Product model Schema

---
+++ backend/models/productModel.js
@@ -0,0 +1,74 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+      trim: true,
+      maxlength: [100, "Product name cannot exceed 100 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter product description"],
+      maxlength: [500, "Product description cannot exceed 500 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please enter product price"],
+      maxLength: [7, "Product price cannot exceed 7 digits"],
+      default: 0.0,
+    },
+    ratings: {
+      type: Number,
+      default: 0,
+    },
+    image: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    category: {
+      type: String,
+      required: [true, "Please enter a product category"],
+    },
+    stock: {
+      type: String,
+      required: [true, "Please enter Product Stock"],
+      maxLength: [5, "Product stock cannot exceed 5 characters"],
+      default: 1,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        name: {
+          type: String,
+          required: [true, "Please enter your name"],
+        },
+        rating: {
+          type: Number,
+          required: [true, "Please enter your rating"],
+        },
+        comment: {
+          type: String,
+          required: [true, "Please enter your comment"],
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Product", productSchema);
