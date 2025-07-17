564788e40aeaaf6eb29ef1acc91ae30d98b06ef3
Ghazanfar Ali <gak_92@hotmail.com>
2025-07-17 13:46:40 +0300

    add more packages and refactor app and server code

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 import express from "express";
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
+
+// Middleware
+app.use(express.json()); // To parse JSON bodies
+
+// Routes
+app.use("/api/v1", productRoutes);
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import {connectDatabase} from "./config/database.js";
 
 dotenv.config({ path: "backend/config/config.env" });
+connectDatabase();
 const PORT = process.env.PORT || 8001;
 
 app.listen(PORT, () => {
