d6d504460f14a663c0c525b3fe4081ab8041cc35
Ghazanfar Ali <gak_92@hotmail.com>
2025-07-26 17:29:12 +0300

    Add functionality to add cartitems into the cart and store in the local storage

--- frontend/src/features/cart/cartSlice.js
+++ frontend/src/features/cart/cartSlice.js
@@ -25,7 +25,7 @@ export const addItemsToCart = createAsyncThunk(
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    cartItems: [],
+    cartItems: JSON.parse(localStorage.getItem("cartItems")) || [],
     loading: false,
     error: null,
     success: false,
@@ -48,12 +48,23 @@ const cartSlice = createSlice({
       })
       .addCase(addItemsToCart.fulfilled, (state, action) => {
         const item = action.payload;
-        state.cartItems.push(item);
+        const existingItem = state.cartItems.find(
+          (i) => i.product === item.product
+        );
+
+        if (existingItem) {
+          existingItem.quantity = item.quantity;
+          // state.cartItems = [...state.cartItems];
+          state.message = `Item ${item.name} already exists in cart. Quantity updated to ${item.quantity}`;
+        } else {
+          state.cartItems.push(item);
+          state.message = `Item ${item.name} added to cart successfully`;
+        }
+
         state.loading = false;
         state.error = null;
         state.success = true;
-        state.message = `Item ${item.name} added to cart successfully`;
-
+        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
       })
       .addCase(addItemsToCart.rejected, (state, action) => {
         state.loading = false;
